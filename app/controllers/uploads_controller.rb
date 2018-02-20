1dfde6fd4d12093ff6e77077f99cc2e0d6a67a30
Kadavar <kadavar32@gmail.com>
2018-02-20 04:28:57 +0200

    add display limit

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,8 +1,13 @@
 class UploadsController < ApplicationController
   def create
     file = params.require(:file)
-    @service_result = UploadsService.new.create(file)
-  rescue ActionController::ParameterMissing => e
+    limit = params[:limit].to_i
+    service_result = UploadsService.new.create(file)
+
+    @resource_type = service_result.resource_type
+    @data = limit > 0 ? service_result.data[0..(limit - 1)] : service_result.data
+    @error = service_result.error
+  rescue ActionController::ParameterMissing => _e
     redirect_to new_upload_path, flash: { error: "File required!" }
   end
 
